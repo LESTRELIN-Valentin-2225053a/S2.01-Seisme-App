f9f8a615b1a1f6a97faaa52e45d35b7b7ffaf8d3
Julesse0 <jules.hospital@etu.univ-amu.fr>
2023-06-05 14:50:34 +0200

    séisme

--- src/main/java/com/example/s201/SeismeApp.java
+++ src/main/java/com/example/s201/SeismeApp.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class SeismeApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SeismeApp.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SeismeApp.class.getResource("SeismeApp.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Séisme!");
         stage.setScene(scene);
         stage.show();
     }
