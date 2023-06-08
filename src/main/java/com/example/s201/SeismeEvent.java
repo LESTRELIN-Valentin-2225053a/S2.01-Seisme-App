7847fd965d218db03d18c852cd8e54cce473de88
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-06-08 11:30:12 +0200

    Initialisation des sliders dans la partie filtrer notamment en liant les deux avec un binding.

--- src/main/java/com/example/s201/SeismeEvent.java
+++ src/main/java/com/example/s201/SeismeEvent.java
@@ -2,6 +2,7 @@ package com.example.s201;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 
@@ -12,6 +13,38 @@ public class SeismeEvent {
     private Label welcomeText;
     @FXML
     private BorderPane root;
+
+    @FXML
+    private Slider sliderMin;
+    @FXML
+    private Slider sliderMax;
+
+
+    //Cette fonction permet d'initialiser les deux sliders contenu dans le filtre
+    public void initialize(){
+        //Initialisation du slider minimum
+        sliderMin.setBlockIncrement(1);
+        sliderMin.setShowTickLabels(true);
+        sliderMin.setShowTickMarks(true);
+        sliderMin.setMajorTickUnit(1);
+        sliderMin.setMinorTickCount(0);
+        sliderMin.setSnapToTicks(true);
+        sliderMin.valueProperty().addListener((observable, oldValue, newValue) -> {
+            int intValue = newValue.intValue();
+        });
+
+        //Initialisation du slider maximum
+        sliderMax.setBlockIncrement(1);
+        sliderMax.setShowTickLabels(true);
+        sliderMax.setShowTickMarks(true);
+        sliderMax.setMajorTickUnit(1);
+        sliderMax.setMinorTickCount(0);
+        sliderMax.setSnapToTicks(true);
+        sliderMax.minProperty().bind(sliderMin.valueProperty());
+        sliderMax.valueProperty().addListener((observable, oldValue, newValue) -> {
+            int intValue = newValue.intValue();
+        });
+    }
 
     //Cette fonction est utilisé par le bouton "Choisir Données" et sert à
     //choisir le fichier csv que l'on veut. La fonction lancera ensuite les
