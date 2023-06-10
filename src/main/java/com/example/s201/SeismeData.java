96fe15f05633232383e8660998acce3d8b6726dd
sookdp <smdbdiop@gmail.com>
2023-06-10 17:08:11 +0200

    Récupération des données pour la LineChart Liaision entre la classe SeismeDate et SeismeEvent Début des instructions pour mettre les données dans la LineChart

--- src/main/java/com/example/s201/SeismeData.java
+++ src/main/java/com/example/s201/SeismeData.java
@@ -4,11 +4,6 @@ import javafx.collections.FXCollections;
 
 import java.io.*;
 import java.net.URL;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,23 +16,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
-import javafx.util.Pair;
 
 public class SeismeData implements Initializable {
 
     @FXML
     private Label moyenne;
-
     private static List<List<String>> Donnees = new ArrayList<>();
     // Les données sur les seismes
 
-    //Ces 4 attributs sont les filtres par defaut par rapport au fichier
-    private static LocalDate dateMin;
-    private static LocalDate dateMax;
-    private static Double intensiteMin;
-    private static Double intensiteMax;
-
-    private static ObservableList<XYChart.Data<String, Number>> DonneesBarchart = FXCollections.observableArrayList();
+    private static ObservableList<XYChart.Data<Number, Number>> DonneesBarchart = FXCollections.observableArrayList();
     //Les données pour le barchart
 
     private static ObservableList<PieChart.Data> DonneesCamembert = FXCollections.observableArrayList(
@@ -46,76 +33,19 @@ public class SeismeData implements Initializable {
             new PieChart.Data("Segment 3", 50)
     );
     // Les données pour le camembert
-
-    private static List<Pair<Number,LocalDate>> dateDonnees = new ArrayList<>();
-    //Les dates contenues dans les données avec leurs identifiants, mais mis tout au format "yyyy/MM/dd",
-    // c'est à dire avec des valeurs par défaut
-    private static ObservableList<XYChart.Data<Number, Number>> DonneesLineChart = FXCollections.observableArrayList();
-    // Les données pour la LineChart
-    public static XYChart.Series<String, Number> SerieDonneesBarchart = new XYChart.Series<>();
+    public static XYChart.Series<Number, Number> SerieDonneesBarchart = new XYChart.Series<>();
     //Contient toutes les données du barchart et permet l'évolution des données;
 
-    public static void minMaxFiltre() throws ParseException {
-        if (Donnees != null && !Donnees.isEmpty()) {
-            List<String> dateString = new ArrayList<>();
-            List<Double> intensiteTri = new ArrayList<>();
-
-            for (int i = 0; i < Donnees.size(); i++) {
-                String tempDateString = Donnees.get(i).get(1);
-                dateString.add(tempDateString);
-                intensiteTri.add(Double.valueOf(Donnees.get(i).get(Donnees.get(i).size() - 2)));
-            }
-
-            Collections.sort(intensiteTri);
-            intensiteMin = intensiteTri.get(0);
-            intensiteMax = intensiteTri.get(intensiteTri.size() - 1);
-
-            for (int index = 0; index < Donnees.size(); index++) {
-                dateDonnees.add(new Pair<>(Integer.valueOf(Donnees.get(index).get(0)), parseDate(dateString.get(index))));
-            }
-
-            // Trie par rapport aux Dates
-            Collections.sort(dateDonnees, new Comparator<Pair<Number, LocalDate>>() {
-                @Override
-                public int compare(Pair<Number, LocalDate> pair1, Pair<Number, LocalDate> pair2) {
-                    return pair1.getValue().compareTo(pair2.getValue());
-                }
-            });
-
-            dateMin = dateDonnees.get(0).getValue();
-            dateMax = dateDonnees.get(dateDonnees.size() - 1).getValue();
-        }
-    }
-
-    private static LocalDate parseDate(String dateStr) {
-        try {
-            if (dateStr.matches("\\d{4}/\\d{2}/\\d{2}")) {
-                return LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-            } else if (dateStr.matches("\\d{4}/\\d{2}")) {
-                return LocalDate.parse(dateStr + "/01", DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-            } else if (dateStr.matches("\\d{4}/")) {
-                return LocalDate.parse(dateStr + "01/01", DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-            }
-        } catch (Exception e) {
-            // Gérer les erreurs de parsing ici
-            System.out.println("Erreur de parsing pour la date : " + dateStr);
-        }
-        return null;
-    }
-
+    private static ObservableList<XYChart.Data<Number, Number>> DonneesLineChart = FXCollections.observableArrayList();
+    // Les données pour la LineChart
 
     //Cette methode permet de lire le fichier CSV donné,puis de le
     // transformer en une liste manipulable
     public static void lectureCSV(File fichier){
         Pattern separateur = Pattern.compile(",(?=([^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-        boolean premiereLigne = true;
         try (BufferedReader csvLecture = new BufferedReader (new FileReader(fichier))) {
             String ligne;
             while ((ligne = csvLecture.readLine()) != null){
-                if (premiereLigne) {
-                    premiereLigne = false;
-                    continue; // Ignorer la première ligne
-                }
                 String[] Seisme = separateur.split(ligne);
                 Donnees.add(Arrays.asList(Seisme));
             }
@@ -131,17 +61,17 @@ public class SeismeData implements Initializable {
         }*/
     }
 
-    public static void prepDonneesBarchart(List<List<String>> Donnees, LocalDate minDate, LocalDate maxDate,
-                                           Double minIntensité, Double maxIntensité) throws ParseException {
+    public void prepDonneesBarchart(List<List<String>> Donnees, int minDate,int maxDate,
+                                    int minIntensité, int maxIntensité){
         if (DonneesBarchart.size() != 0)
             DonneesBarchart.removeAll();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
         for (int i=0; i < Donnees.size(); i+=1){
-            LocalDate tempDate = parseDate(Donnees.get(i).get(1));
-            Double tempIntensité = Double.valueOf(Donnees.get(i).get(Donnees.get(i).size()-2));
+            String[] tempDate = Donnees.get(i).get(1).split("/+");
+            int tempAnnee = Integer.valueOf(tempDate[0]);
+            int tempIntensité = Integer.valueOf(Donnees.get(i).get(Donnees.size()-2));
             if (tempIntensité >= minIntensité && tempIntensité <= maxIntensité
-                    && tempDate.isAfter(minDate) && tempDate.isBefore(maxDate)){
-                DonneesBarchart.add(new XYChart.Data<>(String.valueOf(tempDate), tempIntensité));
+                    && tempAnnee >= minDate && tempAnnee <= maxDate){
+                DonneesBarchart.add(new XYChart.Data<>(tempAnnee, tempIntensité));
             }
         }
     }
@@ -207,43 +137,23 @@ public class SeismeData implements Initializable {
     }
 
 
-    public void prepdonneesCourbe(List<List<String>> Donnees, int minIntensite, int maxIntensite){
-
+    /*
+    * Intensité en fonction de la date
+    * */
+    public void prepdonneesCourbe(List<List<String>> Donnees, int minDate,int maxDate,
+                                  int minIntensite, int maxIntensite){
+        if (DonneesBarchart.size() != 0)
+            DonneesBarchart.removeAll();
+        for (int i=0; i < Donnees.size(); i+=1) {
+            String[] tempDate = Donnees.get(i).get(1).split("/+");
+            int tempAnnee = Integer.valueOf(tempDate[0]);
+            int tempIntensité = Integer.valueOf(Donnees.get(i).get(Donnees.size() - 2));
+            if (tempIntensité >= minIntensite && tempIntensité <= maxIntensite
+                    && tempAnnee >= minDate && tempAnnee <= maxDate) {
+                DonneesBarchart.add(new XYChart.Data<>(tempAnnee, tempIntensité));
+            }
+        }
     }
 
-    public static List<List<String>> getDonnees() {
-        return Donnees;
-    }
-
-    public static LocalDate getDateMin() {
-        return dateMin;
-    }
-
-    public static void setDateMin(LocalDate dateMin) {
-        SeismeData.dateMin = dateMin;
-    }
-
-    public static LocalDate getDateMax() {
-        return dateMax;
-    }
-
-    public static void setDateMax(LocalDate dateMax) {
-        SeismeData.dateMax = dateMax;
-    }
-
-    public static Double getIntensiteMin() {
-        return intensiteMin;
-    }
-
-    public static void setIntensiteMin(Double intensiteMin) {
-        SeismeData.intensiteMin = intensiteMin;
-    }
-
-    public static Double getIntensiteMax() {
-        return intensiteMax;
-    }
-
-    public static void setIntensiteMax(Double intensiteMax) {
-        SeismeData.intensiteMax = intensiteMax;
-    }
 }
+
--- src/main/java/com/example/s201/SeismeEvent.java
+++ src/main/java/com/example/s201/SeismeEvent.java
@@ -1,21 +1,19 @@
 package com.example.s201;
 
 import javafx.fxml.FXML;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
-import javafx.scene.chart.PieChart;
+import com.example.s201.SeismeData;
 
 
 import java.io.File;
-import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.List;
 
-public class SeismeEvent {
+public class SeismeEvent extends SeismeData{
     @FXML
     private Label welcomeText;
     @FXML
@@ -113,15 +111,12 @@ public class SeismeEvent {
     //Cette fonction est utilisé par le bouton "Inserer" et sert à
     //choisir le fichier csv que l'on veut. La fonction lancera ensuite les
     // autres fonctions qui vont préparer les données pour l'affichage.
-    public void insererBouton() throws ParseException {
+    public void insererBouton() {
         FileChooser choixFichier = new FileChooser();
         File fichierCSV = choixFichier.showOpenDialog(root.getScene().getWindow());
         if (fichierCSV != null) {
             //lance la configuration du CSV
             SeismeData.lectureCSV(fichierCSV);
-            SeismeData.minMaxFiltre();
-            SeismeData.prepDonneesBarchart(SeismeData.getDonnees(), SeismeData.getDateMin(), SeismeData.getDateMax(),
-                    SeismeData.getIntensiteMin(), SeismeData.getIntensiteMax());
         } else {
             //Faudra mettre un label qui dit "faut choisir un fichier"
         }
@@ -130,7 +125,7 @@ public class SeismeEvent {
     //Cette méthode est utilisé pour savoir si une date passé en paramètre
     //respecte le bon format classique utilisé en france, 00/00/0000
     public boolean isValidDate(String date) {
-        String datePattern = "^\\d{4}/(0?[1-9]|1[012])/(0?[1-9]|1[0-9]|2[0-9]|3[01])$";
+        String datePattern = "^(0?[1-9]|1[0-9]|2[0-9]|3[01])/(0?[1-9]|1[012])/\\d{4}$";
         return date.matches(datePattern);
     }
 
@@ -154,7 +149,6 @@ public class SeismeEvent {
             dateDebut.setText(null);
         }
         actualiser.setDisable(true);
-
     }
     @FXML
     public void textField2dOnAction() {
@@ -190,7 +184,15 @@ public class SeismeEvent {
     NumberAxis yAxis = new NumberAxis();
 
     LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
-
+    /*Lier les données récoltées à la LineChart :
+    * lineChart.getData().add(prepdonneesCourbe(avec données en paramètre));
+    * Il faut voir s'il faut tracer la LineChart dans cette classe ou bien
+    * dans la classe SeismeData
+    * A déclarer :
+    *   La variable Donnees : private static List<List<String>> Donnees = new ArrayList<>();
+    *       --> à voir comment remplir avec les données
+    *   int minIntensite = Integer.paseInt(sliderMin.getValue())
+    *   int maxIntensite = Integer.paseInt(sliderMax.getValue())*/
     @FXML
     public void courbeOnAction(){
         /* lorsque l'on appuie sur le boutton courbe :
@@ -207,7 +209,9 @@ public class SeismeEvent {
         lineChart.setPrefWidth(1100);
         lineChart.setPrefHeight(200);
         lineChart.setStyle("-fx-background-color: white; -fx-background-radius: 15");
+        //lineChart.getData().add(prepdonneesCourbe()); // ajoute les données à la LineChart
     }
+
     @FXML
     public void diagrammeOnAction(){
         courbeButton.setDisable(false);
@@ -215,6 +219,7 @@ public class SeismeEvent {
         bas.getChildren().clear();
         bas.getChildren().add(diagrammeBandes);
     }
+
     @FXML
     public void actualiserOnAction(){
         /*
